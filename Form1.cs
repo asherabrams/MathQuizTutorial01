3f5c1a02846a7b1e0647f7f0d01ebc03bfaf8fc1
Asher Abrams <asher813@aol.com>
2018-06-07 07:03:23 -0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,23 @@
+﻿// Timed Math Quiz tutorial from MS Visual Studio Docs.
+// This version created 2018-06-07.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuizTutorial01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
