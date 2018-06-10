439d121a1652db6800729cc0d4969b5252f39f8d
Asher Abrams <asher813@aol.com>
2018-06-10 15:42:04 -0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,15 @@ namespace MathQuizTutorial01
 {
     public partial class Form1 : Form
     {
+        // Create a Random object called randomizer 
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+        // These integer variables store the numbers 
+        // for the addition problem. 
+        int addend1;
+        int addend2;
+
         public Form1()
         {
             InitializeComponent();
